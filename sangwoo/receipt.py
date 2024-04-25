3350232b6cf4c02fa73017a3d281beb25e3648f3
Seoree <sangwoo.yu7@gmail.com>
2024-04-25 21:36:21 +0200

    Variance method per receipt

--- sangwoo/receipt.py
+++ sangwoo/receipt.py
@@ -17,6 +17,15 @@ class Receipt:
             scans_data.append([scan.department, scan.time, scan.price])
         return scans_data
 
+    def calculate_time_variance(self):
+        if len(self.scans) < 2:
+            return 0  # Variance requires at least two data points
+        times = [scan.time for scan in self.scans]
+        intervals = [t - s for s, t in zip(times[:-1], times[1:])]
+        mean_interval = sum(intervals) / len(intervals)
+        variance = sum((x - mean_interval) ** 2 for x in intervals) / len(intervals)
+        return variance
+
     def flag_as_sus(self, reason):
         self.sus = True
         self.reasons.append(reason)
